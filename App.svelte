34e7b98c59a8ddcf271d7a4ba8596ca429527169
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2020-09-25 21:21:00 +0200

    Added CDN links

--- App.svelte
+++ App.svelte
@@ -9,7 +9,10 @@ import Packer from "./components/Packer";
   }
 </style>
 <svelte:head>
-<title>SandCraft</title>
+  <title>SandCraft</title>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 </svelte:head>
 <main class="h-full w-full pb-10 text-white">
 <div class="flex flex-row justify-center items-center py-4">
